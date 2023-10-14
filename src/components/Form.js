d1657015ebcf28eba893826cfd940c831f8c28f7
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-10-14 12:06:25 +0300

    add // Exchange to dynamic

---
+++ src/components/Form.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Form(params) {
+    return (
+        <form>
+            <h2 className="label-wrapper">
+                <label htmlFor="new-todo-input" className="label__lg">
+                    Ne yapılacak ?
+                </label>
+            </h2>
+            <input
+                type="text"
+                id="new-todo-input"
+                className="input input__lg"
+                name="text"
+                autoComplete="off"
+            />
+            <button type="submit" className="btn btn__primary btn__lg">
+                Add
+            </button>
+        </form>
+    );
+}
+
+
+export default Form;
+
