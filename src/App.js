ff4d6c80c6abeeb1d8e798bc6337ce380e4f532b
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-10-15 10:50:45 +0300

    add // delete box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import Todo from "./components/Todo";
-// import React from "react";
+import React, { useState, } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
-import React, { useState } from "react";
-import { nanoid } from "nanoid"
+import Todo from "./components/Todo";
+import { nanoid } from "nanoid";
 
 function App(props) {
 
@@ -20,12 +17,36 @@ function App(props) {
     console.log(tasks)
   }
 
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
 
-  const taskList = props.tasks.map((task) => <Todo
-    id={task.id}
-    name={task.name}
-    completed={task.completed}
-    key={task.id} />);
+  const taskList = props.tasks.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+    />
+  ));
+
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
   return (
     <div className="todoapp stack-large">
@@ -37,7 +58,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
